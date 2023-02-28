1f123ad85fd3fcfe2a21a059632eb9c874ceab43
Shreya Surendra Choure <schoure@pdx.edu>
2023-02-27 20:56:54 -0800

    Get books published in the same year

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template, request
 import pickle
 import numpy as np
 
+from recommendations import getBooksYearly
 top_books = pickle.load(open('top_books.pkl', 'rb'))
 
 app = Flask(__name__)
@@ -22,6 +23,10 @@ def recommend_ui():
 def recommend():
     user_input = request.form.get('user_input')
     print(user_input)
+    year = request.form.get('user_input')
+    same_year_books = getBooksYearly(year)
+    print(year)
+    print(same_year_books)
 
     return render_template('searchBooks.html')
 
